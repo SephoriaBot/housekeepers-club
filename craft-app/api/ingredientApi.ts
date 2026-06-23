ca1e01b611140160359f7e52f8e538a638ff352f
SephoriaBot <withallofthisimdone@gmail.com>
2026-06-23 16:34:11 -0400

    Create ingredientApi.ts

---
+++ craft-app/api/ingredientApi.ts
@@ -0,0 +1,13 @@
+import { supabase } from "./supabase";
+
+export async function searchIngredient(ingredient: string) {
+  const { data, error } = await supabase.functions.invoke(
+    "ingredient-search",
+    {
+      body: { ingredient },
+    }
+  );
+
+  if (error) throw error;
+  return data;
+}
