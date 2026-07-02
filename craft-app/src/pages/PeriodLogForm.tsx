e063aecefa4ad405ef151ad365d24bc3b3e11743
SephoriaBot <withallofthisimdone@gmail.com>
2026-07-02 16:30:19 -0400

    Update PeriodLogForm.tsx

--- craft-app/src/pages/PeriodLogForm.tsx
+++ craft-app/src/pages/PeriodLogForm.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { upsertTrackerLog, getTrackerLog } from '../../api/trackerApi';
-import { PeriodValue } from '../types/tracker';
 
 interface Props {
   date: string;
