54717fe72251141a2ac54e48e8bc12632e92d468
SephoriaBot <withallofthisimdone@gmail.com>
2026-07-23 19:53:57 -0400

    Add Lantern component with image and props

---
+++ craft-app/src/components/Lantern.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import lanternImg from "../docs/images/Lantern.png";
+
+type LanternProps = {
+  size?: number;
+  className?: string;
+};
+
+export default function Lantern({ size = 80, className = "" }: LanternProps) {
+  return (
+    <img
+      src={lanternImg}
+      alt="Lantern decoration"
+      className={className}
+      style={{
+        width: size,
+        height: "auto",
+        pointerEvents: "none",
+      }}
+    />
+  );
+}
